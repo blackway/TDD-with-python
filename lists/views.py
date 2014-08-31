baa45ecd3f2f0954ab066938767b15272022ddec
Michael Bright <mjbright@github.com>
2014-08-31 08:59:29 +0200

    Now using template for our home page

--- lists/views.py
+++ lists/views.py
@@ -5,6 +5,6 @@ from django.http import HttpResponse
 
 # TODO: Just a placeholder for now ...
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'home.html')
 
 
