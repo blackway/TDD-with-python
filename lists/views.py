1778240707ca58b51b6329fc307960e463af80ba
Michael Bright <mjbright@github.com>
2014-09-07 22:51:24 +0200

    Chapter6: OK - except func tests under Windows (socket error - windows specific)

--- lists/views.py
+++ lists/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
 from lists.models import Item, List
 
 # Create your views here.
 
-# TODO: Just a placeholder for now ...
 def home_page(request):
     return render(request, 'home.html')
-
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
     list_ = List.objects.create()
@@ -21,4 +15,8 @@ def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/%d/' % (list_.id,))
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
 
