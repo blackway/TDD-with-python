8400d149bfb5d8b3f8512920c6aa059a45a8c8e7
Mike Bright <github@mjbright.net>
2014-09-09 11:53:22 +0200

    Updated functional_tests to be equivalent to chapter06 of the book

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,3 @@
-
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -13,10 +12,12 @@ class NewVisitorTest(LiveServerTestCase):
         # Close the browser now:
         self.browser.quit()
 
+
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
+
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Get the home page of our application:
@@ -27,35 +28,34 @@ class NewVisitorTest(LiveServerTestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        # TODO: You are invited to do a todo item straight away
+        # She is invited to do a todo item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
-        # TODO: You type 'Buy peacock feathers' into a text box
+        # She types 'Buy peacock feathers' into a text box
         inputbox.send_keys('Buy peacock feathers')
 
-        # TODO: When you hit enter, the page updates, and page now lists
-        # TODO: '1: Buy peacock feathers' as an item in the to-do list
+        # When she hits enter, the page updates, and page now lists
+        # '1: Buy peacock feathers' as an item in the to-do list
         inputbox.send_keys(Keys.ENTER)
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        # TODO: There is still a text box inviting heer to add another item
-        # TODO: You enter 'Use peacock feathers to make a fly'
+        # There is still a text box inviting her to add another item
+        # She enters 'Use peacock feathers to make a fly'
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        # TODO: The page updates again and now shows both entries.
-        table = self.browser.find_element_by_id('id_list_table')
+        # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        # Now a new user, Francis, comes along to the site
+        # Now a new user, Francis, comes along to the site.
 
         # We use a new browser sessionto make sure that no information
         # of Edith's is coming through from the cookies etc
@@ -72,16 +72,16 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Francis starts a new list by entering a new item.
         # He is less interesting than Edith...
-        table = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
 
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/lists/.+')
+        self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
 
-        # Again there is no trace of Edith's list
+        # Again, there is no trace of Edith's list
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
@@ -90,10 +90,4 @@ class NewVisitorTest(LiveServerTestCase):
         # TODO: unique URL ... explanatory text to that effect
 
         # TODO: You visit the URL - the to-do list is still there
-
-        # TODO: All is OK
-        self.fail('Finish the test!')
-
-
-
 
